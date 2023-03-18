b7db4f46c876b8ef1fdad4816842de6ee7fcf053
Khiem Le <87161814+Khiem17204@users.noreply.github.com>
2023-03-18 15:17:45 -0400

    Initial commit

---
+++ Photo Scavenger Hunt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photo Scavenger Hunt
+//
+//  Created by Khiem Tran Le on 3/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
